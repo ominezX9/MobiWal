f7f590fdb1620b05b3eaeeb92a2adaeb7ebfaca8
OX99 <winstreakdavid@gmail.com>
2024-10-24 23:53:02 +0100

    update: usersApi endpoint

---
+++ src/api/usersApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "constants";
+import {UserDetailsResponse} from "types/UserDetailsResponse";
+
+const usersApi = createApi({
+    reducerPath: "userApi",
+    baseQuery: fetchBaseQuery({ 
+        baseUrl: BASE_URL,
+     }),
+    tagTypes: ["USER"],
+    endpoints: builder => ({
+        getUser: builder.query<UserDetailsResponse, string>({
+            query: (id) => `users/${id}`,
+            providesTags: ["USER"]
+        }),
+    }),
+})
+
+export default usersApi;
+export const {  
+    useGetUserQuery,
+    useLazyGetUserQuery
+} = usersApi;
