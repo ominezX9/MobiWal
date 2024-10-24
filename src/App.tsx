e6bdedf0fab66508d3bbf95e779122bd15e4ceac
OX99 <winstreakdavid@gmail.com>
2024-10-24 23:51:41 +0100

    update: Router Provider setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Provider } from "react-redux";
-import { store } from "./store";
+import { store } from "store";
 import { RouterProvider } from "react-router-dom";
-
+import routes from "./routes";
 
 export default function App() {
+
+
   return (
     <Provider store={store}>
       <div>
+        <RouterProvider router={routes}/>
       </div>
     </Provider>
   )
