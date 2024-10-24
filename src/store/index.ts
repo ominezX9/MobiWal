071084642601e4263f8eabcb81d20174a2814efd
OX99 <winstreakdavid@gmail.com>
2024-10-24 23:40:24 +0100

    update: setting up auth and users state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userDetails from "./slices/userDetails";
 
+import {
+    authApi,
+    usersApi
+} from "api";
+
+const middleware = [
+    authApi.middleware,
+    usersApi.middleware,
+]
+
 export const store = configureStore({
     reducer: {
+        [authApi.reducerPath]: authApi.reducer,
+        [usersApi.reducerPath]: usersApi.reducer,
         "userDetails" : userDetails
     },
+    middleware(getDefaultMiddleware) { 
+        return getDefaultMiddleware().concat(middleware);
+    }
 })
 
 export type RootState = ReturnType<typeof store.getState>;
