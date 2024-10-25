8ae8184a1a6c0b2704edff40dc0e21080991aef9
OX99 <winstreakdavid@gmail.com>
2024-10-25 07:16:10 +0100

    add: top transaction and intro dashboard

--- src/components/dashboard/amount-card.tsx
+++ src/components/dashboard/amount-card.tsx
@@ -1,15 +1,33 @@
-import React from 'react'
+
 import Transfer from '../../assets/vectors/transfer.svg';
 import Topup from '../../assets/vectors/topup.svg';
 import Bills from '../../assets/vectors/bills.svg';
 import More from '../../assets/vectors/more.svg';
+import { useAppSelector } from '@hooks/redux-hooks';
+import { SessionStorageService } from 'services/SessionStorageService';
+import { useNavigate } from 'react-router-dom';
+import { formatNumber } from '@utils/numberFormatter';
 
 export default function AmountCard() {
+    const userPass = useAppSelector((store) => store.userDetails.password);
+    const userData = SessionStorageService.getItem("user");
+    const navigate = useNavigate();
+
+    if (userData.password === userPass) {
+        // just chill
+        navigate('/dashboard');
+    } else {
+        navigate('/logout')
+    }
+
+
+    // alert(JSON.stringify(user))
     return (
         <div>
+            {JSON.stringify(userPass) + JSON.stringify(userData)}
             <div className="amountblock flex flex-col items-center gap-3 p-20 py-10 pt-[100px]">
                 <h2 className="text-gray text-md">Your Balance</h2>
-                <p className='text-6xl'>N {"12,000"}</p>
+                <p className='text-6xl'>N {formatNumber(userData.balance)}</p>
             </div>
 
             <div className="quickkactions flex justify-around p-10 px-20">
