0680e430cb3864e754870820bd51d014ae17e960
OX99 <winstreakdavid@gmail.com>
2024-10-25 02:22:42 +0100

    fix: redfined for a better routing system

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { 
     createBrowserRouter ,
     createRoutesFromElements,
     Route,
 } from "react-router-dom"
 import ProtectedRoute from "@components/shared/protected-route";
+import DefaultLayout from "layout/layout";
+import Redirect from "@components/shared/redirect";
+import { dashboardRoute } from "./dashboard-routes";
 
+// pages
 const Login = lazy(() => import("@pages/auth/login"))
 
 const appRoutes = (
@@ -14,6 +18,22 @@ const appRoutes = (
         element={<ProtectedRoute/>}
     >
         <Route path="/login" element={<Login/>}/>
+        <Route
+            element={
+                <Suspense
+                    fallback={"Loading..."}
+                >
+                    <DefaultLayout/>
+                </Suspense>
+            }
+        >
+            <Route
+                index 
+                element={<Redirect to="/dashboard" />}
+            />
+            {dashboardRoute()}
+
+        </Route>
 
     </Route>
 )
