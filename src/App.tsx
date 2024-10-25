c9ce80e1fc5c1bca1a07025da52ec6e6e485133e
OX99 <winstreakdavid@gmail.com>
2024-10-25 03:00:30 +0100

    update: custom styles for App dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Provider } from "react-redux";
 import { store } from "store";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
-
+import './App.css';
 export default function App() {
 
 
