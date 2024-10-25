82a85461bbf2543f45e3fb31f540cf111ba73ce3
OX99 <winstreakdavid@gmail.com>
2024-10-25 12:13:30 +0100

    update: isPaying state

--- src/pages/dashboard/transfer.tsx
+++ src/pages/dashboard/transfer.tsx
@@ -134,7 +134,7 @@ export default function Transfer() {
                 )
                 : (
                 <SwipeButton onClick={handleSendMoney}>
-                    Swipe to Send
+                    {isPaying ? "paying ... " : "Swipe to Send" }
                 </SwipeButton>
                 )}
             </div>
