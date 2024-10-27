a663415d8cd14748c99c1e7d4850bfcf94609308
OX99 <winstreakdavid@gmail.com>
2024-10-27 21:05:09 +0100

    update: new changes to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -2,11 +2,12 @@
 import AmountCard from "@components/dashboard/amount-card";
 import TransactionList from "@components/dashboard/transaction-list";
 import HeaderTitle from "@components/shared/header-title";
+import { getUser } from "@utils/getUserFromSession";
 
 export default function Dashboard() {
   return (
     <div className="relative h-full">
-      <HeaderTitle title="" showBackButon={false} />
+      <HeaderTitle title={`Welcome, ${getUser.name}`} showBackButon={false} />
       <AmountCard/>
       <TransactionList/>
     </div>
